ca1be9e6f389ff919a4556be2f374b67907d1b2f
sunil <sunilkdas1961@gmail.com>
2025-03-05 18:34:42 +0530

    Intial add of project

---
+++ ShoppingCart/src/main/java/base/BaseTest.java
@@ -0,0 +1,45 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+
+	public WebDriver driver;
+
+	@Parameters({ "browser" })
+	@BeforeMethod
+	public void beforeTest(String browser) {
+
+		switch (browser) {
+		case "chrome":
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.saucedemo.com/");
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.saucedemo.com/");
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+	@AfterMethod
+	public void afterTest() {
+
+		if (driver != null) {
+			driver.quit();
+
+		}
+
+	}
+}
